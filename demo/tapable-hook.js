50156caee7b9b58f2c4c6ac80603953e2e21c62e
jin <389188533@qq.com>
2019-11-14 22:53:44 +0800

    add demo

--- demo/tapable-hook.js
+++ demo/tapable-hook.js
@@ -37,21 +37,21 @@ const {
 // const data = hook.call('tom');
 // console.log("data: ", data);
 
-const hook = new SyncWaterfallHook(['name', 'age']);
-hook.tap('sync', function(name, age) {
-	console.log(`1-${name}`, age);
-	return [1,2];
-});
-hook.tap('sync', function(name, age) {
-	console.log(`2-${name}`, age);
-	// return 2;
-});
-hook.tap('sync', function(name, age) {
-	console.log(`3-${name}`, age);
-	// return 3;
-});
-const data = hook.call('tom', '11');
-console.log("data: ", data);
+// const hook = new SyncWaterfallHook(['name', 'age']);
+// hook.tap('sync', function(name, age) {
+// 	console.log(`1-${name}`, age);
+// 	return [1,2];
+// });
+// hook.tap('sync', function(name, age) {
+// 	console.log(`2-${name}`, age);
+// 	// return 2;
+// });
+// hook.tap('sync', function(name, age) {
+// 	console.log(`3-${name}`, age);
+// 	// return 3;
+// });
+// const data = hook.call('tom', '11');
+// console.log("data: ", data);
 
 // const hook = new SyncLoopHook(['name']);
 // hook.tap('sync', function(name) {
@@ -74,15 +74,20 @@ console.log("data: ", data);
 // 	console.log("payload one： ", name);
 // 	setTimeout(function() {
 // 		callback(1);
-// 	}, 2000)
+// 	}, 5000)
 // });
 // asyncHook.tap('event-2', function(name, callback) {
 //     console.log("payload two： ", name);
 // 	return 2;
 // });
 // asyncHook.tapPromise('event-2', function(name, callback) {
-//     console.log("payload 3 ", name);
-// 	return Promise.resolve(1);
+// 	console.log("payload 3 ", name);
+	
+// 	return new Promise(function(resolve,reject) {
+// 		setTimeout(() => {
+// 			resolve(1);
+// 		}, 3000);
+// 	});
 // });
 // asyncHook.tapAsync('event-1', function(name, callback) {
 // 	console.log("payload 5 ", name);
@@ -92,12 +97,43 @@ console.log("data: ", data);
 //     console.log("payload 4 ", name);
 // 	return Promise.resolve(2);
 // });
-// asyncHook.callAsync(1, function(data) {
+// asyncHook.callAsync(2, function(data) {
 // 	console.log('call: ', data);
 // });
-// asyncHook.promise(1).then(function(data) {
-// 	console.log("promise: ", data);
+// asyncHook.promise(3).then(d => {
+// 	console.log("data: ", d);
 // });
+
+const asyncHook = new AsyncSeriesWaterfallHook(['name']);
+console.time("time");
+asyncHook.tapAsync('event-1', function(name, callback) {
+	console.log("payload 1 ", name);
+	setTimeout(function() {
+		callback(4);
+	}, 1000)
+});
+// asyncHook.tap('event-2', function(name, callback) {
+//     console.log("payload 2 ", name);
+// 	// return 2;
+// });
+// asyncHook.tapPromise('event-2', function(name, callback) {
+//     console.log("payload 3 ", name);
+// 	return Promise.resolve(3);
+// });
+// asyncHook.tapPromise('event-3', function(name, callback) {
+//     console.log("payload 4 ", name);
+// 	return Promise.resolve(4);
+// });
+asyncHook.tapAsync('event-2', function(name, callback) {
+	console.log("payload 5 ", name);
+	setTimeout(function() {
+		callback(5);
+	}, 3000)
+});
+asyncHook.callAsync(3, function(data) {
+	console.log('call: ', data);
+	console.timeEnd("time");
+});
 
 // const asyncHook = new AsyncParallelBailHook(['name']);
 // asyncHook.tapAsync('event-1', function(name, callback) {
@@ -108,7 +144,7 @@ console.log("data: ", data);
 // });
 // asyncHook.tap('event-2', function(name, callback) {
 //     console.log("payload 2 ", name);
-// 	// return 2;
+// 	return 2;
 // });
 // asyncHook.tapPromise('event-2', function(name, callback) {
 //     console.log("payload 3 ", name);
@@ -122,6 +158,6 @@ console.log("data: ", data);
 // 	console.log("payload 5 ", name);
 // 	callback(5);
 // });
-// asyncHook.callAsync(1, function(data) {
+// asyncHook.callAsync(2, function(data) {
 // 	console.log('call: ', data);
 // });
--- md/webpack/webpack-tapable.md
+++ md/webpack/webpack-tapable.md
@@ -15,14 +15,14 @@ PS：实例化 Hook 需要一个字符串数组作为参数，字符串数组中
 类型 | 特点
 :- | - 
 SyncHook | 回调串行执行，return 不能中断回调队列，没有返回值
-SyncBailHook | 回调串行执行，return 能中断回调队列，返回值为return值
-SyncWaterfallHook | 回调串行执行，return不能中断回调队列，前面回调返回值会作为后面回调的入参，最后一个return值为调用结果返回值
+SyncBailHook | 回调串行执行，事件处理函数返回值不为 undefined 能中断回调队列，返回值为return值
+SyncWaterfallHook | 回调串行执行，return不能中断回调队列，上一个事件处理函数返回值会作为后面回调的入参，最后一个return值为调用结果返回值
 SyncLoopHook | 回调串行执行，return返回值不为 undefined 时会重复执行回调（重复执行第一个到当前回调），没有返回值
-AsyncParallelHook | 异步并行执行，只有 tapAsync 有返回值，返回值为最先调用 callback 传递的值 
-AsyncParallelBailHook |
-AsyncSeriesHook | 
-AsyncSeriesBailHook | 
-AsyncSeriesWaterfallHook |
+AsyncParallelHook | 异步并行执行
+AsyncParallelBailHook | 异步并行执行，事件处理函数有返回值时不继续往后执行
+AsyncSeriesHook | 异步串行执行
+AsyncSeriesBailHook | 异步串行执行，事件处理函数有返回值时不继续往后执行
+AsyncSeriesWaterfallHook | 异步串行执行，上一个事件处理函数返回值作为参数传递给下一个事件处理函数
 
 ### Hook 函数注册和调用方式
 类型 | 注册方式 | 调用方式
