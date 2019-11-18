1d7926e0174338909f05d5935252345cfb2c5c26
cjjin <cjjin@streamax.com>
2019-11-18 15:56:30 +0800

    add

--- demo/test.js
+++ demo/test.js
@@ -1,1 +1,22 @@
-console.log(process.env);
+const cp = require('child_process');
+
+console.log(process.argv);
+const ls = cp.spawn('node', ['./demo/tapable-hook.js'], {
+    env: {
+        name: 1,
+        age: 2,
+        addr: 3
+    }
+});
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+    console.log(`子进程退出，退出码 ${code}`);
+});
